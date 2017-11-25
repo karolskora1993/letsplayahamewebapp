5e2409eb9330e3277177d3f99832f188a26a1784
karolskora1993 <karol-skora@poczta.pl>
2017-11-25 12:07:46 +0100

    fix errors

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 var express = require("express");
 var bodyParser = require("body-parser")
-var Client = require('pg')
-
+const { Client } = require('pg')
 
 var app = express();
 app.use(bodyParser.json());
