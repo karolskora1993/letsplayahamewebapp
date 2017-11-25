c914eade6f7594370a0396437b856d36143f3f39
karolskora1993 <karol-skora@poczta.pl>
2017-11-25 11:31:57 +0100

    add db connection

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require("express");
-bodyParser = require("body-parser")
+var bodyParser = require("body-parser")
+var Client = require('pg')
+var consts = require('./app_consts.js')
+
 app.use(bodyParser.json());
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
 
 var app = express();
 var server = app.listen(process.env.PORT || 8080, function () {
-    var port = server.address().port;
-    console.log("App now running on port", port);
-  });
+
+});
